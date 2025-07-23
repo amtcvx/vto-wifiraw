32e9de1aefaf185e3c54f691262b9791e22684e8
pprz <you@example.com>
2025-07-23 17:06:36 +0200

    up

--- src/wfb.c
+++ src/wfb.c
@@ -21,9 +21,11 @@ extern wfb_utils_down_t wfb_utils_down[2];
 /*****************************************************************************/
 int main(void)
 {
+/*
   int8_t raw;
+*/
   uint8_t tmp, devout, readcpt=0, devcpt, readtab[FD_NB];
-  uint8_t num=0, seq=0;
+/*  uint8_t num=0, seq=0;
   uint64_t exptime;
   ssize_t len;
 
@@ -62,212 +64,215 @@ int main(void)
 #endif // BOARD
        
   wfb_utils_stat_t wfbstat;
+*/
   wfb_utils_init_t dev[FD_NB];
   struct pollfd readsets[FD_NB];
   bool bckup = false;
   wfb_utils_init(dev, &readcpt, readtab, readsets, &bckup);
-  if (!bckup) wfbstat.raw[0] = 0;
-  else {
-    wfbstat.raw[0] = 0; wfbstat.raw[1] = 1;
-#if RAW
-#if BOARD
-    downmsg[0]->iov_base = &wfb_utils_down[0];
-    downmsg[1]->iov_base = &wfb_utils_down[1];
-    downmsg[0]->iov_len = sizeof(wfb_utils_down_t);
-    downmsg[1]->iov_len = sizeof(wfb_utils_down_t);
-    wfb_utils_down[0].chan = 100 + dev[wfbstat.raw[1]+RAW0_FD].raw.freqcptcur;
-    wfb_utils_down[1].chan = dev[wfbstat.raw[0]+RAW0_FD].raw.freqcptcur;
-#endif // BOARD
-#endif // RAW
-  }
-
-  for(;;) {	
-    if (0 != poll(readsets, readcpt, -1)) {
-      for (uint8_t cpt=0; cpt<readcpt; cpt++) {
-        if (readsets[cpt].revents == POLLIN) {
-          devcpt = readtab[ cpt ];
-          switch(devcpt) {
-            case TIME_FD :  
-              len = read( dev[devcpt].fd, &exptime, sizeof(uint64_t));
-              wfb_utils_periodic(dev, bckup, downmsg, &wfbstat);
-      	      break; 
-#if BOARD
-            case VID1_FD:
-            case VID2_FD:
-	      if (vidnum[devcpt - VID1_FD] < FEC_K) {
-                ptrmsg = &msg[0][devcpt - WFB_FD][vidnum[devcpt - VID1_FD]];
-		memset(ptrmsg->iov_base, 0, ONLINE_MTU);
-                ptrmsg->iov_base = &msgbuf[0][devcpt - WFB_FD][vidnum[devcpt - VID1_FD]][sizeof(wfb_utils_fec_t)];
-                ptrmsg->iov_len  = PAY_MTU;
-                len = readv( dev[devcpt].fd, ptrmsg, 1);
-                ptrmsg->iov_base = &msgbuf[0][devcpt - WFB_FD][vidnum[devcpt - VID1_FD]][0];
-		ptrmsg->iov_len = len + sizeof(wfb_utils_fec_t);
-                memcpy(&msgbuf[0][devcpt - WFB_FD][vidnum[devcpt - VID1_FD]][0], &len, sizeof(wfb_utils_fec_t));
-		vidnum[devcpt - VID1_FD]++;
-/*
-                pdebug = (uint8_t *)&msgbuf[0][devcpt - WFB_FD][vidnum[devcpt - VID1_FD] - 1][sizeof(wfb_utils_fec_t)];
-                printf("readv fec(%d)  %02X[%ld]%02X\n", vidnum[devcpt - VID1_FD] - 1,
-                  *(14 + pdebug), 
-		  len,
-                  *(len + pdebug - 1));
-*/
-	      }
-	      break;
-#else
-#endif // BOARD
-     
-#if TELEM
-            case TEL_FD:
-#endif // TELEM
-	    case TUN_FD:
-	      ptrmsg = &msg[0][devcpt - WFB_FD][0];
-              ptrmsg->iov_len = ONLINE_MTU;
-              ptrmsg->iov_len = readv( dev[devcpt].fd, ptrmsg, 1);
-	      break;
-
-            case RAW0_FD:
-            case RAW1_FD:
-              wfb_utils_presetrawmsg(&rawmsg[devcpt - RAW0_FD][rawcur], ONLINE_MTU, true);
-              len = recvmsg( dev[devcpt].fd, &rawmsg[devcpt - RAW0_FD][rawcur].msg, MSG_DONTWAIT);
-              if (!((len > 0)&&(wfb_utils_pay.droneid == DRONEID))) {
-#if BOARD
-	        wfbstat.stat[devcpt - RAW0_FD].incoming++;
-#endif // BOARD
-	      } else {
-
-                if (numprev[devcpt - RAW0_FD] >= 0) {
-		  if (numprev[devcpt - RAW0_FD] == 255) {
-		    if (wfb_utils_pay.num != 0) {
-  		      wfbstat.stat[devcpt - RAW0_FD].fails++;
-		    }
-		  } else if ((1 + (numprev[devcpt - RAW0_FD])) != wfb_utils_pay.num) {
-  		    wfbstat.stat[devcpt - RAW0_FD].fails++;
-  		  } 
-  		}
-
-		numprev[devcpt - RAW0_FD] = wfb_utils_pay.num;
-		devout = WFB_FD + wfb_utils_pay.msgcpt;
-		if (wfb_utils_pay.msglen > 0) {
-		  switch(devout) {
-#if BOARD
-#else
-		    case VID1_FD:
-		    case VID2_FD:
-                      wfb_utils_dispatchvideo(&dev[devout], &(wfbstat.stat[devcpt - RAW0_FD].dev[devout]), 
-				              &rawmsg[devcpt - RAW0_FD][rawcur], &dspvid[devout - VID1_FD], fec_p);
-                      if (rawcur == (rawmsgstoresize - 1)) rawcur=0;
-                      else rawcur++;
-		      break;
-
-                    case WFB_FD:
-                      wfbstat.stat[devcpt - RAW0_FD].incoming++;
-		      wfbstat.stat[devcpt - RAW0_FD].dev[devout].rcv += wfb_utils_pay.msglen;
-		      wfbstat.stat[devcpt - RAW0_FD].chan = 
-		        ((wfb_utils_down_t *)rawmsg[devcpt - RAW0_FD][rawcur].headvecs.head[wfb_utils_datapos].iov_base)->chan;
-	              break;
-#endif // BOARD
-#if TELEM
-                    case TEL_FD:
-#if BOARD
-#else
-		      if ((len = sendto(dev[devout].fd,
-		        rawmsg[devcpt - RAW0_FD][rawcur].headvecs.head[wfb_utils_datapos].iov_base,
-		        wfb_utils_pay.msglen,
-		        MSG_DONTWAIT, (struct sockaddr *)&(dev[devout].addrout), sizeof(struct sockaddr))) > 0) {
-		          wfbstat.stat[devcpt - RAW0_FD].dev[devout].rcv += len;
-		      }
-#endif // BOARD
-#endif // TELEM
-                    case TUN_FD:
-                      if ((len = write(dev[devout].fd, 
-		        rawmsg[devcpt - RAW0_FD][rawcur].headvecs.head[wfb_utils_datapos].iov_base,
-		        wfb_utils_pay.msglen)) > 0) {
-		          wfbstat.stat[devcpt - RAW0_FD].dev[devout].rcv += len;
-		      }
-
-                      break;
-
-                    default:
-                      break;
-		  }
-	        }
-	      }
-	      break;
-
-	    default:  
-              break;
-          }
-        } // if readset POLLIN
-      } // end for 
-
-      for (uint8_t j=0; j < msgnb; j++) {
-        if (bckup) tmp=2; else tmp=1;
-
-        for (uint8_t i=0; i<tmp; i++) {
-	  uint8_t k=0;
-#if BOARD
-	  if ((j == (VID1_FD - WFB_FD)) || (j == (VID2_FD - WFB_FD))) {
-	    if (vidnum[ j - VID1_FD + WFB_FD] == FEC_K) {
-              for (uint8_t f=0; f<FEC_K; f++) datablocks[f] = (uint8_t *)&msgbuf[i][j][f];
-              for (uint8_t f=0; f<(FEC_N - FEC_K); f++) {
-	        fecblocks[f] = (uint8_t *)&msgbuf[i][j][f + FEC_K];
-                msg[i][j][f + FEC_K].iov_len = ONLINE_MTU;
-	      }
-	      fec_encode(fec_p,
-			 (const gf*restrict const*restrict const)datablocks,
-			 (gf*restrict const*restrict const)fecblocks,
-			 (const unsigned*restrict const)blocknums, (FEC_N-FEC_K), ONLINE_MTU);
-	    }
-	  } 
-	  if ((j == (VID1_FD - WFB_FD)) || (j == (VID2_FD - WFB_FD)))
-	    k = (vidnum[ j - VID1_FD + WFB_FD ] - 1);
-#endif // BOARD
-          while (msg[i][j][k].iov_len > 0) {
-	    raw = wfbstat.raw[i];
-            if (raw < 0) msg[i][j][k].iov_len = 0; 
-	    else {
-#if BOARD
-	      if ((j == (VID1_FD - WFB_FD)) || (j == (VID2_FD - WFB_FD)))
-	        seq = vidseq[ j - VID1_FD + WFB_FD];
-#endif // BOARD
-              wfb_utils_presetrawmsg(&rawmsg[raw][0], ONLINE_MTU, false);
-              wfb_utils_pay.msgcpt = j;
-              wfb_utils_pay.droneid = DRONEID;
-              wfb_utils_pay.seq = seq;
-              wfb_utils_pay.fec = k;
-              wfb_utils_pay.num = num++;
-              wfb_utils_pay.msglen = msg[i][j][k].iov_len ;
-  
-      	      rawmsg[raw][0].headvecs.head[wfb_utils_datapos] = msg[i][j][k];
-  #if RAW
-  #else
-      	      rawmsg[raw][0].msg.msg_name = &dev[raw + RAW0_FD].addrout;
-      	      rawmsg[raw][0].msg.msg_namelen = sizeof(dev[raw + RAW0_FD].addrout);
-  #endif // RAW
-              len = sendmsg(dev[raw + RAW0_FD].fd, &rawmsg[raw][0].msg, MSG_DONTWAIT);
-        	      wfbstat.stat[raw].dev[j + WFB_FD].snd += msg[i][j][k].iov_len ;
-/*
-              pdebug = (uint8_t *)rawmsg[raw][0].headvecs.head[wfb_utils_datapos].iov_base;
-              printf("sendmsg num(%d) seq(%d) fec(%d) len(%ld) %02X[%ld]%02X\n",num-1,seq,k,len,
-                  *(16 + pdebug),
-                  rawmsg[raw][0].headvecs.head[wfb_utils_datapos].iov_len,
-                  *(rawmsg[raw][0].headvecs.head[wfb_utils_datapos].iov_len + pdebug - 1));
-*/
-#if BOARD
-	      if ((j == (VID1_FD - WFB_FD)) || (j == (VID2_FD - WFB_FD))) {
-                if ((k == (FEC_N - 1)) && (msg[i][j][FEC_N - 1].iov_len > 0)) {
-		  vidnum[ j - VID1_FD + WFB_FD] = 0; 
-		  vidseq[ j - VID1_FD + WFB_FD]++; 
-                }
-	      }
-#endif // BOARD
-              msg[i][j][k].iov_len = 0;
-	      k++;
-	    }
-	  }
-        }
-      }
-//      if (seq==5) exit(-1); DEBUG
-    } // end poll
-  }
+//
+//  if (!bckup) wfbstat.raw[0] = 0;
+//  else {
+//    wfbstat.raw[0] = 0; wfbstat.raw[1] = 1;
+//#if RAW
+//#if BOARD
+//    downmsg[0]->iov_base = &wfb_utils_down[0];
+//    downmsg[1]->iov_base = &wfb_utils_down[1];
+//    downmsg[0]->iov_len = sizeof(wfb_utils_down_t);
+//    downmsg[1]->iov_len = sizeof(wfb_utils_down_t);
+//    wfb_utils_down[0].chan = 100 + dev[wfbstat.raw[1]+RAW0_FD].raw.freqcptcur;
+//    wfb_utils_down[1].chan = dev[wfbstat.raw[0]+RAW0_FD].raw.freqcptcur;
+//#endif // BOARD
+//#endif // RAW
+//  }
+//
+//  for(;;) {	
+//    if (0 != poll(readsets, readcpt, -1)) {
+//      for (uint8_t cpt=0; cpt<readcpt; cpt++) {
+//        if (readsets[cpt].revents == POLLIN) {
+//          devcpt = readtab[ cpt ];
+//          switch(devcpt) {
+//            case TIME_FD :  
+//              len = read( dev[devcpt].fd, &exptime, sizeof(uint64_t));
+//              wfb_utils_periodic(dev, bckup, downmsg, &wfbstat);
+//      	      break; 
+//#if BOARD
+//            case VID1_FD:
+//            case VID2_FD:
+//	      if (vidnum[devcpt - VID1_FD] < FEC_K) {
+//                ptrmsg = &msg[0][devcpt - WFB_FD][vidnum[devcpt - VID1_FD]];
+//		memset(ptrmsg->iov_base, 0, ONLINE_MTU);
+//                ptrmsg->iov_base = &msgbuf[0][devcpt - WFB_FD][vidnum[devcpt - VID1_FD]][sizeof(wfb_utils_fec_t)];
+//                ptrmsg->iov_len  = PAY_MTU;
+//                len = readv( dev[devcpt].fd, ptrmsg, 1);
+//                ptrmsg->iov_base = &msgbuf[0][devcpt - WFB_FD][vidnum[devcpt - VID1_FD]][0];
+//		ptrmsg->iov_len = len + sizeof(wfb_utils_fec_t);
+//                memcpy(&msgbuf[0][devcpt - WFB_FD][vidnum[devcpt - VID1_FD]][0], &len, sizeof(wfb_utils_fec_t));
+//		vidnum[devcpt - VID1_FD]++;
+///*
+//                pdebug = (uint8_t *)&msgbuf[0][devcpt - WFB_FD][vidnum[devcpt - VID1_FD] - 1][sizeof(wfb_utils_fec_t)];
+//                printf("readv fec(%d)  %02X[%ld]%02X\n", vidnum[devcpt - VID1_FD] - 1,
+//                  *(14 + pdebug), 
+//		  len,
+//                  *(len + pdebug - 1));
+//
+//	      }
+//	      break;
+//#else
+//#endif // BOARD
+//     
+//#if TELEM
+//            case TEL_FD:
+//#endif // TELEM
+//	    case TUN_FD:
+//	      ptrmsg = &msg[0][devcpt - WFB_FD][0];
+//              ptrmsg->iov_len = ONLINE_MTU;
+//              ptrmsg->iov_len = readv( dev[devcpt].fd, ptrmsg, 1);
+//	      break;
+//
+//            case RAW0_FD:
+//            case RAW1_FD:
+//              wfb_utils_presetrawmsg(&rawmsg[devcpt - RAW0_FD][rawcur], ONLINE_MTU, true);
+//              len = recvmsg( dev[devcpt].fd, &rawmsg[devcpt - RAW0_FD][rawcur].msg, MSG_DONTWAIT);
+//              if (!((len > 0)&&(wfb_utils_pay.droneid == DRONEID))) {
+//#if BOARD
+//	        wfbstat.stat[devcpt - RAW0_FD].incoming++;
+//#endif // BOARD
+//	      } else {
+//
+//                if (numprev[devcpt - RAW0_FD] >= 0) {
+//		  if (numprev[devcpt - RAW0_FD] == 255) {
+//		    if (wfb_utils_pay.num != 0) {
+//  		      wfbstat.stat[devcpt - RAW0_FD].fails++;
+//		    }
+//		  } else if ((1 + (numprev[devcpt - RAW0_FD])) != wfb_utils_pay.num) {
+//  		    wfbstat.stat[devcpt - RAW0_FD].fails++;
+//  		  } 
+//  		}
+//
+//		numprev[devcpt - RAW0_FD] = wfb_utils_pay.num;
+//		devout = WFB_FD + wfb_utils_pay.msgcpt;
+//		if (wfb_utils_pay.msglen > 0) {
+//		  switch(devout) {
+//#if BOARD
+//#else
+//		    case VID1_FD:
+//		    case VID2_FD:
+//                      wfb_utils_dispatchvideo(&dev[devout], &(wfbstat.stat[devcpt - RAW0_FD].dev[devout]), 
+//				              &rawmsg[devcpt - RAW0_FD][rawcur], &dspvid[devout - VID1_FD], fec_p);
+//                      if (rawcur == (rawmsgstoresize - 1)) rawcur=0;
+//                      else rawcur++;
+//		      break;
+//
+//                    case WFB_FD:
+//                      wfbstat.stat[devcpt - RAW0_FD].incoming++;
+//		      wfbstat.stat[devcpt - RAW0_FD].dev[devout].rcv += wfb_utils_pay.msglen;
+//		      wfbstat.stat[devcpt - RAW0_FD].chan = 
+//		        ((wfb_utils_down_t *)rawmsg[devcpt - RAW0_FD][rawcur].headvecs.head[wfb_utils_datapos].iov_base)->chan;
+//	              break;
+//#endif // BOARD
+//#if TELEM
+//                    case TEL_FD:
+//#if BOARD
+//#else
+//		      if ((len = sendto(dev[devout].fd,
+//		        rawmsg[devcpt - RAW0_FD][rawcur].headvecs.head[wfb_utils_datapos].iov_base,
+//		        wfb_utils_pay.msglen,
+//		        MSG_DONTWAIT, (struct sockaddr *)&(dev[devout].addrout), sizeof(struct sockaddr))) > 0) {
+//		          wfbstat.stat[devcpt - RAW0_FD].dev[devout].rcv += len;
+//		      }
+//#endif // BOARD
+//#endif // TELEM
+//                    case TUN_FD:
+//                      if ((len = write(dev[devout].fd, 
+//		        rawmsg[devcpt - RAW0_FD][rawcur].headvecs.head[wfb_utils_datapos].iov_base,
+//		        wfb_utils_pay.msglen)) > 0) {
+//		          wfbstat.stat[devcpt - RAW0_FD].dev[devout].rcv += len;
+//		      }
+//
+//                      break;
+//
+//                    default:
+//                      break;
+//		  }
+//	        }
+//	      }
+//	      break;
+//
+//	    default:  
+//              break;
+//          }
+//        } // if readset POLLIN
+//      } // end for 
+//
+//      for (uint8_t j=0; j < msgnb; j++) {
+//        if (bckup) tmp=2; else tmp=1;
+//
+//        for (uint8_t i=0; i<tmp; i++) {
+//	  uint8_t k=0;
+//#if BOARD
+//	  if ((j == (VID1_FD - WFB_FD)) || (j == (VID2_FD - WFB_FD))) {
+//	    if (vidnum[ j - VID1_FD + WFB_FD] == FEC_K) {
+//              for (uint8_t f=0; f<FEC_K; f++) datablocks[f] = (uint8_t *)&msgbuf[i][j][f];
+//              for (uint8_t f=0; f<(FEC_N - FEC_K); f++) {
+//	        fecblocks[f] = (uint8_t *)&msgbuf[i][j][f + FEC_K];
+//                msg[i][j][f + FEC_K].iov_len = ONLINE_MTU;
+//	      }
+//	      fec_encode(fec_p,
+//			 (const gf*restrict const*restrict const)datablocks,
+//			 (gf*restrict const*restrict const)fecblocks,
+//			 (const unsigned*restrict const)blocknums, (FEC_N-FEC_K), ONLINE_MTU);
+//	    }
+//	  } 
+//	  if ((j == (VID1_FD - WFB_FD)) || (j == (VID2_FD - WFB_FD)))
+//	    k = (vidnum[ j - VID1_FD + WFB_FD ] - 1);
+//#endif // BOARD
+//          while (msg[i][j][k].iov_len > 0) {
+//	    raw = wfbstat.raw[i];
+//            if (raw < 0) msg[i][j][k].iov_len = 0; 
+//	    else {
+//#if BOARD
+//	      if ((j == (VID1_FD - WFB_FD)) || (j == (VID2_FD - WFB_FD)))
+//	        seq = vidseq[ j - VID1_FD + WFB_FD];
+//#endif // BOARD
+//              wfb_utils_presetrawmsg(&rawmsg[raw][0], ONLINE_MTU, false);
+//              wfb_utils_pay.msgcpt = j;
+//              wfb_utils_pay.droneid = DRONEID;
+//              wfb_utils_pay.seq = seq;
+//              wfb_utils_pay.fec = k;
+//              wfb_utils_pay.num = num++;
+//              wfb_utils_pay.msglen = msg[i][j][k].iov_len ;
+//  
+//      	      rawmsg[raw][0].headvecs.head[wfb_utils_datapos] = msg[i][j][k];
+//  #if RAW
+//  #else
+//      	      rawmsg[raw][0].msg.msg_name = &dev[raw + RAW0_FD].addrout;
+//      	      rawmsg[raw][0].msg.msg_namelen = sizeof(dev[raw + RAW0_FD].addrout);
+//  #endif // RAW
+//              len = sendmsg(dev[raw + RAW0_FD].fd, &rawmsg[raw][0].msg, MSG_DONTWAIT);
+//        	      wfbstat.stat[raw].dev[j + WFB_FD].snd += msg[i][j][k].iov_len ;
+///*
+//              pdebug = (uint8_t *)rawmsg[raw][0].headvecs.head[wfb_utils_datapos].iov_base;
+//              printf("sendmsg num(%d) seq(%d) fec(%d) len(%ld) %02X[%ld]%02X\n",num-1,seq,k,len,
+//                  *(16 + pdebug),
+//                  rawmsg[raw][0].headvecs.head[wfb_utils_datapos].iov_len,
+//                  *(rawmsg[raw][0].headvecs.head[wfb_utils_datapos].iov_len + pdebug - 1));
+//
+//#if BOARD
+//	      if ((j == (VID1_FD - WFB_FD)) || (j == (VID2_FD - WFB_FD))) {
+//                if ((k == (FEC_N - 1)) && (msg[i][j][FEC_N - 1].iov_len > 0)) {
+//		  vidnum[ j - VID1_FD + WFB_FD] = 0; 
+//		  vidseq[ j - VID1_FD + WFB_FD]++; 
+//                }
+//	      }
+//#endif // BOARD
+//              msg[i][j][k].iov_len = 0;
+//	      k++;
+//	    }
+//	  }
+//        }
+//      }
+////      if (seq==5) exit(-1); DEBUG
+//    } // end poll
+//  }
+//
 }
--- src/wfb.h
+++ src/wfb.h
@@ -25,14 +25,23 @@
 static int PORTS_VID[] = { 5600, 5700 };
 static int PORTS_RAW[] = { 3000, 3500 };
 
-static int INITCHAN[] = { 12, 32 };
+static int INITCHAN[][2] = { { 12, 32 }, { 14 , 35 } };
 
 #define WFBPORT  5000
 
 #define TELPORTUP 4245
 #define TELPORTDOWN 4244
 
-typedef enum { TIME_FD, RAW0_FD, RAW1_FD, WFB_FD, TUN_FD, VID1_FD, VID2_FD, TEL_FD, FD_NB } cannal_t;
+#if BOARD
+typedef enum { TIME_FD, RAW0_FD, RAW1_FD, 
+	WFB_FD, TUN_FD, TEL_FD, VID_FD, 
+	FD_NB } cannal_t;
+#else 
+typedef enum { TIME_FD, RAW0_FD, RAW1_FD, RAW2_FD, RAW3_FD,
+	WFB1_FD, TUN1_FD, TEL1_FD, VID1_FD,
+	WFB2_FD, TUN2_FD, TEL2_FD, VID2_FD,
+	FD_NB } cannal_t;
+#endif // BOARD
 
 // cat /proc/sys/net/core/rmem_max rmem_default 212992 < 1400 x 154
 #define STORE_SIZE	5
