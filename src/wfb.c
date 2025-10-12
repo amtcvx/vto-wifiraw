05d4ec4935717a0044ab6b4b4ed7c9a169418457
pprz <Vous@exemple.com>
2025-10-12 07:54:48 +0200

    up

--- src/wfb.c
+++ src/wfb.c
@@ -90,12 +90,9 @@ int main(void) {
 	  }
 #endif // BOARD
         
-	  printf("(%d)\n",cpt);
-
           for (uint8_t cptraw = minraw; cptraw < maxraw; cptraw++) {
-
             if (u.readtab[cpt] == cptraw) {
-					   
+
               wfb_utils_heads_pay_t headspay;
               memset(&headspay,0,sizeof(wfb_utils_heads_pay_t));
               memset(&rawbuf[rawcur][0],0,ONLINE_MTU);
@@ -103,12 +100,12 @@ int main(void) {
               struct iovec iovheadpay = { .iov_base = &headspay, .iov_len = sizeof(wfb_utils_heads_pay_t) };
               struct iovec iovpay = { .iov_base = &rawbuf[rawcur][0], .iov_len = ONLINE_MTU };
 #if RAW
-              struct iovec iovtab[5] = { iov_radiotaphd_rx, iov_ieeehd_rx, iov_llchd_rx, iovheadpay, iovpay };
+              struct iovec iovtab[5] = { iov_radiotaphd_rx, iov_ieeehd_rx, iov_llchd_rx, iovheadpay, iovpay }; uint8_t msglen = 5;
 	      memset(iov_llchd_rx.iov_base, 0, sizeof(iov_llchd_rx));
 #else 
-              struct iovec iovtab[2] = { iovheadpay, iovpay };
+              struct iovec iovtab[2] = { iovheadpay, iovpay }; uint8_t msglen = 2;
 #endif // RAW
-              struct msghdr msg = { .msg_iov = iovtab, .msg_iovlen = sizeof(iovtab) };
+              struct msghdr msg = { .msg_iov = iovtab, .msg_iovlen = msglen };
               len = recvmsg(u.fd[u.socktab[cptraw]], &msg, MSG_DONTWAIT) - sizeof(wfb_utils_heads_pay_t);
 
               if (len > 0) {
@@ -177,9 +174,6 @@ int main(void) {
 #endif // RAW
   	      struct msghdr msg = { .msg_iov = iovtab, .msg_iovlen = msglen, .msg_name = &u.norawoutaddr, .msg_namelen = sizeof(u.norawoutaddr) };
               len = sendmsg(u.fd[ c + minraw ], (const struct msghdr *)&msg, MSG_DONTWAIT);
-
-	      printf("(%d)(%ld)\n",d,len);
-
 	      lentab[d][c] = 0;
 #if BOARD
               if ((d == WFB_VID)&&(vidcur == 0)&&(k == (FEC_N-1))) sequence++;
