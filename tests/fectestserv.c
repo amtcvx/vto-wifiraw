7ab4a191e900824b2aa76021abdf55adb851d375
pprz <you@example.com>
2025-10-22 14:03:54 +0200

    up

--- tests/fectestserv.c
+++ tests/fectestserv.c
@@ -139,6 +139,9 @@ i*/
 
 	  uint8_t dum=0;
 
+//            if (((sequence == 2) && ((k == 8)||(k == 9)||(k == 10)||(k == 11))) || (sequence == 3) || (sequence == 4) || ((sequence == 5) && ((k == 0)||(k == 1)||(k == 2)||(k == 3)||(k == 4)||(k == 5)||(k == 6)))) dum=1;
+//            if (((sequence == 2) && ((k == 8)||(k == 9)||(k == 10)||(k == 11))) || (sequence == 3) || (sequence == 4) || ((sequence == 5) && ((k == 0)||(k == 1)||(k == 2)||(k == 3)))) dum=1;
+
 //            if ((sequence == 3) && ((k == 5)||(k == 8)||(k == 9)||(k == 10)||(k == 11))) dum=1;
 //            if ((sequence == 3) && ((k == 7)||(k == 8)||(k == 9)||(k == 10)||(k == 11))) dum=1;
 //            if ((sequence == 3) && ((k == 0)||(k == 8)||(k == 9)||(k == 10)||(k == 11))) dum=1;
@@ -156,18 +159,19 @@ i*/
 //            if ((sequence == 2) && (k == 2)) dum=1;
             
 //            if ((sequence == 2) && (k == 0)) dum=1;
+
 //            else 
 
 	    rawlen = sendmsg(rawfd, (const struct msghdr *)&msg, MSG_DONTWAIT);
-/*
+
 	  if (k<FEC_K) {
 	    uint8_t *ptr = vidbuf[k]; ssize_t tmp;
 	    if (k<FEC_K) tmp = ((wfb_utils_fec_t *)ptr)->feclen; else tmp = ONLINE_MTU;
-            printf("(%d] len(%ld) ",k,tmp);
+            printf("[%d][%d] len(%ld) ",sequence,k,tmp);
             for (uint8_t i=0;i<5;i++) printf("%x ",*(ptr+i));printf(" ... ");
             for (uint16_t i=tmp-5;i<tmp;i++) printf("%x ",*(ptr+i));printf("\n");
 	  }
-*/
+
 	  vidlen = 0;
           if ((vidcur == 0)&&(k == (FEC_N-1))) { sequence++; } //printf("\n"); }
 	}
